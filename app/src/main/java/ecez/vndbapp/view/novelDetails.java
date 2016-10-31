090b295917cc7ee229311c3fafa061b44a18b944
John Teng <john.teng97@gmail.com>
2016-10-30 23:19:18 -0400

    Improved novel details asthetics
    Added dividing lines between labels and changed colors/sizes of various
    text

--- app/src/main/java/ecez/vndbapp/view/novelDetails.java
+++ app/src/main/java/ecez/vndbapp/view/novelDetails.java
@@ -1,9 +1,14 @@
 package ecez.vndbapp.view;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -28,6 +33,11 @@ public class novelDetails extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_novel_details);
+
+        Window window = getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.setStatusBarColor(Color.GRAY);
 
         title = (TextView)findViewById(R.id.quickstats_title);
         developer = (TextView) findViewById(R.id.quickstats_developer);
