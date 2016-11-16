0cc83eedbb555e322c09f3f883024f6287c4968d
John Teng <john.teng97@gmail.com>
2016-11-15 23:37:00 -0500

    Added close button
    Added a close button to the image detail activity to close it

--- app/src/main/java/ecez/vndbapp/view/imageActivity.java
+++ app/src/main/java/ecez/vndbapp/view/imageActivity.java
@@ -3,6 +3,8 @@ package ecez.vndbapp.view;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
@@ -12,6 +14,7 @@ import uk.co.senab.photoview.PhotoViewAttacher;
 
 public class imageActivity extends AppCompatActivity {
     ImageView image;
+    Button closeButton;
     PhotoViewAttacher attacher;
     String imageURL;
 
@@ -25,9 +28,19 @@ public class imageActivity extends AppCompatActivity {
         imageURL = intent.getStringExtra("IMAGE_URL");
         image = (ImageView)findViewById(R.id.fullscreen_image);
 
+        closeButton = (Button)findViewById(R.id.close_button);
+
+        closeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
         Picasso.with(getApplicationContext()).load(imageURL).into(image);
         attacher = new PhotoViewAttacher(image);
 
 
     }
+
+
 }
