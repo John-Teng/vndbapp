9f1fbbfecd832421c3dfbcaec8b563cec2b1dc6c
John Teng <john.teng97@gmail.com>
2016-11-28 23:40:04 -0500

    Character general details
    completed the character profile activity's general details layout

---
+++ app/src/main/java/ecez/vndbapp/view/CharacterProfile.java
@@ -0,0 +1,15 @@
+package ecez.vndbapp.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ecez.vndbapp.R;
+
+public class CharacterProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_character_profile);
+    }
+}
