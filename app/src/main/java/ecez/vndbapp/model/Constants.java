31f8bd9e396dc456d3475fd5c56b0941299df8f8
John Teng <john.teng97@gmail.com>
2017-04-24 00:03:03 -0400

    Forgot to add the newly added classes

---
+++ app/src/main/java/ecez/vndbapp/model/Constants.java
@@ -0,0 +1,12 @@
+package ecez.vndbapp.model;
+
+/**
+ * Created by johnteng on 2017-04-23.
+ */
+
+public class Constants {
+
+    public static final String serverErrorResponse = "Error response from the server";
+    public static final String serverNotLoggedInError = "Not logged into server";
+    public static final String gsonSerializationError = "Problem occurred with json serialization";
+}
---
+++ app/src/main/java/ecez/vndbapp/model/Error.java
@@ -0,0 +1,20 @@
+package ecez.vndbapp.model;
+
+/**
+ * Created by johnteng on 2017-04-23.
+ */
+
+public class Error {
+    private String id;
+    private String msg;
+
+    public Error () {};
+
+    public String getId() {
+        return id;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+}
