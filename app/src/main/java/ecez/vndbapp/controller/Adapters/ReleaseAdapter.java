219ce5d6e88897fafe2ade5443c7e97cea7a9d28
John Teng <john.teng97@gmail.com>
2017-05-22 12:29:28 -0400

    Added languge and console info to the Release List

--- app/src/main/java/ecez/vndbapp/controller/Adapters/ReleaseAdapter.java
+++ app/src/main/java/ecez/vndbapp/controller/Adapters/ReleaseAdapter.java
@@ -2,11 +2,14 @@ package ecez.vndbapp.controller.Adapters;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.support.v7.widget.RecyclerView;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import ecez.vndbapp.R;
@@ -61,7 +64,9 @@ public class ReleaseAdapter extends RecyclerView.Adapter<ReleaseAdapter.holder>{
             holder.titleText.setText(item.getTitle());
             if (item.getPatch())
                 holder.titleText.append(" (Patch)");
-            holder.dateText.append(item.getDate());
+            holder.dateText.setText("Released:   " + item.getDate());
+            loadConsoleIcon(item.getPlatforms()[0], holder.platformText, holder.platformIcon);
+            loadCountryIcon(item.getLanguages()[0], holder.languageText, holder.countryIcon);
         }
 
         @Override
@@ -69,16 +74,148 @@ public class ReleaseAdapter extends RecyclerView.Adapter<ReleaseAdapter.holder>{
             return releases.length;
         }
 
-        class holder extends RecyclerView.ViewHolder {
+    private void loadCountryIcon (String country, TextView text, ImageView image) {
+        switch (country) {
+            case "en":
+                text.setText("English");
+                image.setImageResource(R.drawable.uk);
+                break;
+            case "de":
+                text.setText("German");
+                image.setImageResource(R.drawable.germany);
+                break;
+            case "es":
+                text.setText("Spanish");
+                image.setImageResource(R.drawable.spain);
+                break;
+            case "it":
+                text.setText("Italian");
+                image.setImageResource(R.drawable.italy);
+                break;
+            case "ja":
+                text.setText("Japanese");
+                image.setImageResource(R.drawable.japan);
+                break;
+            case "ko":
+                text.setText("Korean");
+                image.setImageResource(R.drawable.korea);
+                break;
+            case "ru":
+                text.setText("Russian");
+                image.setImageResource(R.drawable.russia);
+                break;
+            case "zh":
+                text.setText("Chinese");
+                image.setImageResource(R.drawable.china);
+                break;
 
-            private TextView titleText, dateText;
-
-            public holder(View itemView) {
-                super(itemView);
-                titleText = (TextView)itemView.findViewById(R.id.release_list_item_title);
-                dateText = (TextView)itemView.findViewById(R.id.release_list_item_date);
-            }
+            case "vi":
+                text.setText("Vietnamese");
+                image.setImageResource(R.drawable.vietnam);
+                break;
+            case "fr":
+                text.setText("French");
+                image.setImageResource(R.drawable.france);
+                break;
+            default:
+                text.setText(country.toUpperCase());
+                image.setImageResource(R.drawable.eu);
         }
+    }
+
+    private void loadConsoleIcon (String console, TextView text, ImageView image) {
+        switch (console) {
+            case "gba":
+                image.setImageResource(R.drawable.gba_logo);
+                text.setText("");
+                break;
+            case "nds":
+                image.setImageResource(R.drawable.nds_logo);
+                text.setText("");
+                break;
+            case "n3d":
+                image.setImageResource(R.drawable.n3ds_logo);
+                text.setText("");
+                break;
+            case "psp":
+                image.setImageResource(R.drawable.psp_logo);
+                text.setText("");
+                break;
+            case "ps2":
+                image.setImageResource(R.drawable.ps2_logo);
+                text.setText("");
+                break;
+            case "ps3":
+                image.setImageResource(R.drawable.ps3_logo);
+                text.setText("");
+                break;
+            case "ps4":
+                image.setImageResource(R.drawable.ps4_logo);
+                text.setText("");
+                break;
+            case "psv":
+                image.setImageResource(R.drawable.psvita_logo);
+                text.setText("");
+                break;
+            case "xb3":
+                image.setImageResource(R.drawable.xbox360_logo);
+                text.setText("");
+                break;
+            case "win":
+                image.setImageResource(R.drawable.windows_logo);
+                image.getLayoutParams().height = (int) convertDpToPixel(20);
+                text.setText("");
+                break;
+            case "ios":
+                image.setImageResource(R.drawable.ios_logo);
+                image.getLayoutParams().height = (int) convertDpToPixel(18);
+                text.setText("");
+                break;
+            case "wii":
+                image.setImageResource(R.drawable.wii_logo);
+                image.getLayoutParams().height = (int) convertDpToPixel(18);
+                text.setText("");
+                break;
+            case "and":
+                image.setImageResource(R.drawable.android_logo);
+                image.getLayoutParams().height = (int) convertDpToPixel(20);
+                text.setText("");
+                break;
+            case "lin":
+                image.setImageResource(R.drawable.linux_logo);
+                image.getLayoutParams().height = (int) convertDpToPixel(20);
+                text.setText("");
+                break;
+            default:
+                image.setImageResource(R.drawable.game_controller_logo);
+                text.setText(console.toUpperCase());
+                break;
+        }
+    }
+
+    public float convertDpToPixel(float dp){
+        Resources resources = context.getResources();
+        DisplayMetrics metrics = resources.getDisplayMetrics();
+        float px = dp * ((float)metrics.densityDpi / DisplayMetrics.DENSITY_DEFAULT);
+        return px;
+    }
+
+    class holder extends RecyclerView.ViewHolder {
+
+        private TextView titleText, dateText, languageText, platformText;
+        private ImageView countryIcon, platformIcon;
+
+        public holder(View itemView) {
+            super(itemView);
+            titleText = (TextView)itemView.findViewById(R.id.release_list_item_title);
+            dateText = (TextView)itemView.findViewById(R.id.release_list_item_date);
+            languageText = (TextView)itemView.findViewById(R.id.release_list_language);
+            platformText = (TextView)itemView.findViewById(R.id.release_list_console);
+            countryIcon = (ImageView)itemView.findViewById(R.id.release_list_country_icon);
+            platformIcon = (ImageView)itemView.findViewById(R.id.release_list_console_icon);
+
+        }
+    }
 
 
 }
